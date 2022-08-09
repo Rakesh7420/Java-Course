a9eb60872c13f1f39baf29e0e2047c2867387b08
Rakesh Gudur <rakeshgudur7420@gmail.com>
2022-08-09 05:41:41 +0530

    java

---
+++ index.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class index {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
