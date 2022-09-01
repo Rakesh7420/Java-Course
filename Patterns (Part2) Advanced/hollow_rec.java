cd8489bdfb5c329525b8b5a42e02352c605a7782
Rakesh Gudur <rakeshgudur7420@gmail.com>
2022-09-01 23:37:56 +0530

    hollow_rec

---
+++ Patterns (Part2) Advanced/hollow_rec.java
@@ -0,0 +1,19 @@
+public class hollow_rec {
+    public static void main(String[] args) {
+        hollowr_ectangle(4, 5);
+    }
+
+    public static void hollowr_ectangle(int n, int m) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || i == n || j == 1 || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
